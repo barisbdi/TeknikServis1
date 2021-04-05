109302af8d9e814050b0f81b7aed568b9060a879
BARIS\baris <barisbdci@gmail.com>
2021-04-05 17:05:42 +0300

    Program başlangıç formu ayarlandı.

--- DXApplication1/Form2.Designer.cs
+++ DXApplication1/Form2.Designer.cs
@@ -90,6 +90,7 @@ namespace DXApplication1
             // 
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
             // 
             // panel3
             // 
